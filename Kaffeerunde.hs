e641306a301e7d523f199816f9fff51a54049155
Fabio Madge-Pimentel <fabio@madge.me>
2012-10-13 17:55:23 +0200

    whole sceleton

--- Kaffeerunde.hs
+++ Kaffeerunde.hs
@@ -11,9 +11,16 @@ type Staff   = [Employe]
 type Avg     = Double
 type Avgs    = [Avg]
 type Alpha   = Int
+type TheWholeThing = (Staff, Avgs, Alpha, Pot)
 
 kaf :: Data -> Data
-kaf d = d
+kaf d = extractData(interval(interval(interval(makeTheWholeThing d))))
+
+extractData :: TheWholeThing -> Data
+extractData (a, b, c, d) = (a, b, c)
+
+makeTheWholeThing :: Data -> TheWholeThing
+makeTheWholeThing (a, b, c) = ((addDay a), b, c, 10)
 
 emptyData :: Alpha -> Data
 emptyData a = ((emptyStaff 15 []), [], a)
@@ -24,3 +31,15 @@ emptyStaff i s = emptyStaff (i-1) (emptyEmploye : s)
 
 emptyEmploye :: Employe
 emptyEmploye = []
+
+interval :: TheWholeThing -> TheWholeThing
+interval a = a
+
+interaction :: Employe -> Pot -> (Employe, Pot)
+interaction a b = (a, b)
+
+happy :: Employe -> Bool
+happy a = True
+
+addDay :: Staff -> Staff
+addDay d = d
