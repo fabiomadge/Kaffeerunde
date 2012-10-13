ad6e8f4d311072e046063a305073a7ac71646565
Fabio Madge-Pimentel <fabio@madge.me>
2012-10-13 17:23:17 +0200

    finished the ui

--- Kaffeerunde.hs
+++ Kaffeerunde.hs
@@ -1,15 +1,26 @@
 module Kaffeerunde where
 
-type Data = (Staff, Avgs, Alpha)
-type Pot = Int
-type Cups = Int
-type MPots = Int
-type Day = (Cups, MPots)
-type Days = [Day]
+type Data    = (Staff, Avgs, Alpha)
+type Pot     = Int
+type Cups    = Int
+type MPots   = Int
+type Day     = (Cups, MPots)
+type Days    = [Day]
 type Employe = Days
-type Staff = [Employe]
-type Avg = Double
-type Avgs = [Avg]
-type Alpha = Int
+type Staff   = [Employe]
+type Avg     = Double
+type Avgs    = [Avg]
+type Alpha   = Int
 
---kaf :: Data -> Data
+kaf :: Data -> Data
+kaf d = d
+
+emptyData :: Alpha -> Data
+emptyData a = ((emptyStaff 15 []), [], a)
+
+emptyStaff :: Int -> Staff -> Staff
+emptyStaff 0 s = s
+emptyStaff i s = emptyStaff (i-1) (emptyEmploye : s)
+
+emptyEmploye :: Employe
+emptyEmploye = []
--- UI.hs
+++ UI.hs
@@ -3,8 +3,17 @@ module UI where
 import Kaffeerunde
 
 result :: Alpha -> IO ()
-result a = do
-	putStrLn (show (a))
+result a = step(kaf (emptyData a))
+
+step :: Data -> IO ()
+step (a, b, c) = do
+	putStrLn (show(av b))
+	if ((length b) < 100)
+		then step(kaf (a, b, c))
+		else putStrLn ("Done")
+
+av :: Avgs -> Avg
+av a = (sum a) / fromIntegral (length a)
 
 readAlpha :: IO (Alpha)
 readAlpha = do
