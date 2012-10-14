0fc53b4a897340f5d0b40bb0ae60e56b23dec8e5
Fabio Madge-Pimentel <fabio@madge.me>
2012-10-14 19:46:02 +0200

    more or less final version -> seams to work

--- Kaffeerunde.hs
+++ Kaffeerunde.hs
@@ -1,5 +1,10 @@
 module Kaffeerunde where
 
+import System.Random.Shuffle
+import System.Random
+
+type Gen     = StdGen
+type Gens    = (Gen, Gen, Gen)
 type Data    = (Staff, Avgs, Alpha)
 type Pot     = Int
 type Cups    = Int
@@ -10,17 +15,21 @@ type Employe = Days
 type Staff   = [Employe]
 type Avg     = Double
 type Avgs    = [Avg]
-type Alpha   = Int
+type Alpha   = Double
 type TheWholeThing = (Staff, Avgs, Alpha, Pot)
 
-kaf :: Data -> Data
-kaf d = extractData(interval(interval(interval(makeTheWholeThing d))))
+kaf :: Data -> Gens -> Data
+kaf d gs = calcAvg (extractData (interval (interval (interval (makeTheWholeThing d) (g1 gs)) (g2 gs)) (g3 gs)))
+	where 
+		g1 (a, b, c) = a
+		g2 (a, b, c) = b
+		g3 (a, b, c) = c
 
 extractData :: TheWholeThing -> Data
 extractData (a, b, c, d) = (a, b, c)
 
 makeTheWholeThing :: Data -> TheWholeThing
-makeTheWholeThing (a, b, c) = ((addDay a), b, c, 10)
+makeTheWholeThing (a, b, c) = ((addDay a), b, c, 0)
 
 emptyData :: Alpha -> Data
 emptyData a = ((emptyStaff 15 []), [], a)
@@ -32,14 +41,58 @@ emptyStaff i s = emptyStaff (i-1) (emptyEmploye : s)
 emptyEmploye :: Employe
 emptyEmploye = []
 
-interval :: TheWholeThing -> TheWholeThing
-interval a = a
+interval :: TheWholeThing -> Gen -> TheWholeThing
+interval (a, b, c, d) g = foldTHW(recint((sa), b, c, d))
+	where
+		sa = shuffle' a (length a) g
 
-interaction :: Employe -> Pot -> (Employe, Pot)
-interaction a b = (a, b)
+recint :: TheWholeThing -> [TheWholeThing]
+recint ([], b, c, d) = [([], b, c, d)]
+recint (a, b, c, d)  = ([newEmp], b, c, newPot) : recint (tail a, b, c, newPot)
+	where
+		newEmp = fst (interaction (head a) d c)
+		newPot = snd (interaction (head a) d c)
 
-happy :: Employe -> Bool
-happy a = True
+foldTHW :: [TheWholeThing] -> TheWholeThing
+foldTHW (a) = (foldEmps, ags, alps, lPot)
+	where
+		ags = qsnd (head a)
+		alps = qthd (head a)
+		lPot = qfth (last a)
+		foldEmps = foldr (++) [] (map qfst a)
+		qfst (w, x, y, z) = w
+		qsnd (w, x, y, z) = x
+		qthd (w, x, y, z) = y
+		qfth (w, x, y, z) = z
+
+interaction :: Employe -> Pot -> Alpha -> (Employe, Pot)
+interaction e p a
+	| length e < 10                        = (drinkCup, p)       --ignition phase(automatic refill)
+	| length e == 10 && happy e a && p > 0 = (drinkCup, usePot)  --drink and leave
+	| p == 0 && happy e a                  = (fillAndDrink, 9)  --make new coffee and drink
+	| p == 0 && not (happy e a)            = (e, p)              --emtpy machine and to unhappy to make new coffee
+	| otherwise                            = (e, p)              --base case
+		where
+			drinkCup     = (oldCups + 1, snd (head e)) : tail e
+			fillAndDrink = (oldCups + 1, snd (head e) + 1) : tail e
+			usePot       = (p - 1)
+			oldCups      = fst (head e)
+
+happy :: Employe -> Alpha -> Bool
+happy e a
+	| cSum >= 10 && (fromIntegral (cSum) < a || fromIntegral (pSum) < a) = True
+	| otherwise = False
+		where
+			cSum = sum (map fst e)
+			pSum = sum (map snd e)
 
 addDay :: Staff -> Staff
-addDay d = d
+addDay d = map add d
+	where add l =  (0, 0) :(take 9 l)
+
+calcAvg :: Data -> Data
+calcAvg (a, b, c) = (a, ((daylyAvg a) : b), c)
+
+daylyAvg :: Staff -> Avg
+daylyAvg s = fromIntegral (sum a) / fromIntegral (length a)
+	where a = map fst(map head(s))
--- UI.hs
+++ UI.hs
@@ -1,15 +1,21 @@
 module UI where
 
 import Kaffeerunde
+import System.Random
 
 result :: Alpha -> IO ()
-result a = step(kaf (emptyData a))
+result a = do
+	gs <- getGens
+	putStrLn ("3.0")
+	step(kaf (emptyData a) gs)
 
 step :: Data -> IO ()
 step (a, b, c) = do
-	putStrLn (show(av b))
+	gs <- getGens
+	putStrLn (show(head b))
 	if ((length b) < 100)
-		then step(kaf (a, b, c))
+		then do 
+			step(kaf (a, b, c) gs)
 		else putStrLn ("Done")
 
 av :: Avgs -> Avg
@@ -21,3 +27,10 @@ readAlpha = do
 	al <- getLine
 	let parsedAl = (read al)::Alpha
 	return parsedAl
+
+getGens :: IO (Gens)
+getGens = do
+	g1 <- newStdGen
+	g2 <- newStdGen
+	g3 <- newStdGen
+	return (g1, g2, g3)
